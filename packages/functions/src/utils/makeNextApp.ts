77dd3d610e40e1823a17f61b04239584a57c3614
Witsarut Sawetkanit <illumina-ova@live.com>
2019-11-21 18:20:08 +0700

    fix typo

--- packages/functions/src/utils/makeNextApp.ts
+++ packages/functions/src/utils/makeNextApp.ts
@@ -8,7 +8,7 @@ export const DEV = process.env.NODE_ENV !== 'production'
 export const makeNextApp = ({ dir = '.', distDir = '.next' }) => {
   let app: ReturnType<typeof next> | null = null
   return functions.https.onRequest(async (req: any, res: any) => {
-    const me = app || (app = next({ def: DEV, dir, conf: { distDir }}))
+    const me = app || (app = next({ dev: DEV, dir, conf: { distDir }}))
     
     const handle = me.getRequestHandler()
     await me.prepare()
